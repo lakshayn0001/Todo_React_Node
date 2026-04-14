42d680ecbb01c17721cbba12994785845637df45
Lakshay <lakshay@192.168.1.12>
2026-04-14 12:51:01 +0530

    folder structure changes

--- Browser/src/App.jsx
+++ Browser/src/App.jsx
@@ -4,14 +4,22 @@ import { useState } from 'react'
 import './App.css'
 import { Creating } from './Todo_Creation/Creating'
 import TodoData from './Todo_Creation/TodoData'
+import { useEffect } from 'react'
 
 function App() {
 
-const [data,setData]=useState([])
+const [data,setData]=useState(()=>{
+  const store=localStorage.getItem("todo")
+  return store ? JSON.parse(store): []
+})
 
 const handleData=(e)=>{
   setData([...data,e])
 }
+
+useEffect(()=>{
+  localStorage.setItem("todo",JSON.stringify(data))
+},[data])
 
 console.log(data)
   return (
